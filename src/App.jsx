5db44e24897163bc2eb9550792e5cb8bef02045d
Mohammed-Afeef-Sayeed <afeefsayeed4.4@gmail.com>
2024-11-02 12:42:44 +0530

    Added Components and modified home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Learn from "./components/Learn";
+import Practice from "./components/Practice";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="h-screen bg-black text-white">
+    <div
+      className="min-h-screen bg-[url('./assets/images/bg.jpg')] text-white
+      bg-cover bg-center bg-no-repeat"
+    >
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/practice" element={<Practice />} />
+      </Routes>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -9,11 +10,11 @@ const Navbar = () => {
             <a className="text-[23px] font-bold bg-white px-4 py-2 rounded-t-lg border-red-500 border-x-4 border-y-2 text-black">
               <h1 className="">Logo</h1>
             </a>
-            <div className="flex gap-7 text-lg font-bold">
-              <a href="">Home</a>
-              <a href="">About</a>
-              <a href="">Learn</a>
-              <a href="">Practice</a>
+            <div className="hidden md:flex gap-7 text-lg font-bold">
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/learn">Learn</Link>
+              <Link to="/practice">Practice</Link>
             </div>
             <div>
               <button className="bg-green-500 px-4 py-2 text-xl font-bold shadow-lg shadow-green-200 text-black rounded-lg active:scale-[0.8]">
