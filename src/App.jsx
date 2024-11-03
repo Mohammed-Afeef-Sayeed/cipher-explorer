aeb55961e8dc69d6ade551aaa0c3335b26794a6b
k-amith1610 <2110030372@klh.edu.in>
2024-11-03 17:58:46 +0530

    Completed UI of Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,30 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Learn from "./components/Learn";
 import Practice from "./components/Practice";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 const App = () => {
+
+  const location = useLocation();
+
   return (
     <>
       <div
         className="z-0 absolute w-full min-h-screen bg-[url('./assets/images/cipher-gif.gif')] text-white
         bg-cover bg-center bg-no-repeat"
       >
-        <Navbar />
+        {/* <Navbar /> */}
+        {location.pathname !== "/login" && location.pathname !== "/signup" && <Navbar />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/learn" element={<Learn />} />
           <Route path="/practice" element={<Practice />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-// import { FaLock } from "react-icons/fa";
+import React from 'react';
 import { MdSecurity } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="flex flex-col lg:flex-row min-h-screen text-white bg-black">
+      {/* Left Side - Image */}
       <div className="w-full lg:w-1/2 h-[250px] md:h-[350px] lg:h-screen bg-[url('./assets/images/cipher-gif.gif')] 
-            bg-cover bg-no-repeat bg-center">
+            bg-cover bg-no-repeat bg-center lg:order-2">
       </div>
       
-       {/* Right Side - Login Form */}
-       <div className="lg:w-1/2 w-full flex items-center justify-center p-8 lg:p-16 bg-gray-900 bg-opacity-90 z-10">
+      {/* Right Side - Signup Form */}
+      <div className="lg:w-1/2 w-full flex items-center justify-center p-8 lg:p-16 bg-gray-900 bg-opacity-90 z-10 lg:order-1">
         <div className="w-full max-w-md p-6 md:p-10 rounded-lg shadow-lg animate-slideIn">
           <div className="flex flex-col items-center mb-6 animate-fadeIn">
             <MdSecurity className="text-5xl text-green-500 mb-4 animate-pulse" />
-            <h2 className="text-3xl font-semibold">Login</h2>
+            <h2 className="text-3xl font-semibold">Signup</h2>
           </div>
           <form className="space-y-5">
+            <div>
+              <label htmlFor="username" className="block text-gray-400 font-semibold">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="w-full p-3 border border-gray-700 bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+                placeholder="Enter your username"
+              />
+            </div>
             <div>
               <label htmlFor="email" className="block text-gray-400 font-semibold">
                 Email
@@ -44,17 +55,16 @@ const Login = () => {
               type="submit"
               className="w-full bg-green-600 text-white p-3 rounded-lg font-semibold hover:bg-green-700 transition duration-300 transform hover:scale-[1.02]"
             >
-              Login
+              Signup
             </button>
           </form>
           <div className="py-3 px-1">
-            <p className="text-[14px] font-semibold">Don't have an account? <Link to="/signup"><span className="text-blue-400 hover:text-blue-500">Signup</span></Link></p>
+            <p className="text-[14px] font-semibold">Already have an account? <Link to="/login"><span className="text-blue-400 hover:text-blue-500">Login</span></Link></p>
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Login
+export default Signup;
