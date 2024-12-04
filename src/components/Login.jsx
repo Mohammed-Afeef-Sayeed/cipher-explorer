7cfa598072f65a4af91c4aa9cabca817606284d8
k-amith1610 <2110030372@klh.edu.in>
2024-12-04 18:39:29 +0530

    Connected Login with server

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 // import { FaLock } from "react-icons/fa";
 import { MdSecurity } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const payload = {
+        email,
+        password,
+      }
+
+      const response = await axios.post(
+        "http://localhost:8080/api/login",
+        payload,
+        {
+          withCredentials: true, 
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setEmail("");
+      setPassword("");
+      console.log(response);
+      alert(response.data.message);
+      console.log(response.data.user);
+      navigate("/");
+
+    } catch (error) {
+      console.log(error);
+      alert(error.response.data.error);
+    }
+  }
+
   return (
     <div className="flex flex-col lg:flex-row min-h-screen text-white bg-black">
       <div className="w-full lg:w-1/2 h-[250px] md:h-[350px] lg:h-screen bg-[url('./assets/images/cipher-gif.gif')] 
@@ -17,7 +57,7 @@ const Login = () => {
             <MdSecurity className="text-5xl text-green-500 mb-4 animate-pulse" />
             <h2 className="text-3xl font-semibold">Login</h2>
           </div>
-          <form className="space-y-5">
+          <form onSubmit={(e) => handleSubmit(e)} className="space-y-5">
             <div>
               <label htmlFor="email" className="block text-gray-400 font-semibold">
                 Email
@@ -27,6 +67,8 @@ const Login = () => {
                 id="email"
                 className="w-full p-3 border border-gray-700 bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
                 placeholder="Enter your email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
               />
             </div>
             <div>
@@ -38,6 +80,8 @@ const Login = () => {
                 id="password"
                 className="w-full p-3 border border-gray-700 bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
                 placeholder="Enter your password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
               />
             </div>
             <button
